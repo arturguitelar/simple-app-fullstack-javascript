b87e891832de46c2a55965a173d10a11a58b25f0
Artur Guitelar <artur.guitelar@gmail.com>
2025-01-06 13:13:16 -0300

    Add crud functions to Home form

--- frontend/user-registration/src/pages/Home/index.jsx
+++ frontend/user-registration/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './style.css';
 import ReactIcon from '../../assets/react.svg';
 import api from '../../services/api';
@@ -6,9 +6,29 @@ import api from '../../services/api';
 function Home() {
   const [users, setUsers] = useState([]);
 
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
   async function getUsers() {
     const usersFromApi = await api.get('/users');
     setUsers(usersFromApi.data);
+  }
+
+  async function createUser() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+
+    getUsers();
+  }
+
+  async function removeUser(id) {
+    await api.delete(`/users/${id}`);
+
+    getUsers();
   }
 
   useEffect(() => {
@@ -22,10 +42,12 @@ function Home() {
           <img src={ReactIcon} />
           Register
         </h1>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="age" placeholder="Age" />
-        <input type="text" name="email" placeholder="Email" />
-        <button type="button">Send</button>
+        <input type="text" name="name" placeholder="Name" ref={inputName} />
+        <input type="text" name="age" placeholder="Age" ref={inputAge} />
+        <input type="text" name="email" placeholder="Email" ref={inputEmail} />
+        <button type="button" onClick={createUser}>
+          Send
+        </button>
       </form>
 
       {users.map((user) => (
@@ -35,7 +57,7 @@ function Home() {
             <p>Age: {user.age}</p>
             <p>Email: {user.email}</p>
           </div>
-          <button>remove</button>
+          <button onClick={() => removeUser(user.id)}>remove</button>
         </div>
       ))}
     </div>
